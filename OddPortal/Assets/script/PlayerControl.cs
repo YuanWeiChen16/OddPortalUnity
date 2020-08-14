918b78a297d9e2980ef02949fa25744afef0d5eb
Zasxxsdc <wficta137@gmail.com>
2020-08-14 21:38:26 +0800

    update 0814 add camera portal move

--- OddPortal/Assets/script/PlayerControl.cs
+++ OddPortal/Assets/script/PlayerControl.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-
+    public GameObject Camera_Obj;
     public float Walkspeed;
     public float rotatonspeed;
     public bool inv;
+    public Transform distence;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     // Update is called once per frame
     private void FixedUpdate()
     {
+
         Vector3 p_Velocity = new Vector3();
         Vector3 R_R = new Vector3();
         if (Input.GetKey(KeyCode.W))
@@ -42,6 +44,8 @@ public class PlayerControl : MonoBehaviour
         {
             R_R -= new Vector3(0, 1, 0);
         }
+
+
         Vector3 nowpos = this.GetComponent<Transform>().position;
         if (inv == false)
         {
@@ -51,8 +55,8 @@ public class PlayerControl : MonoBehaviour
         {
             nowpos -= p_Velocity * Walkspeed * Time.deltaTime;
         }
-        this.GetComponent<Transform>().position = nowpos;
-        this.GetComponent<Transform>().Rotate(R_R*rotatonspeed*Time.deltaTime);
+        this.transform.position = nowpos;
+        this.transform.Rotate(R_R * rotatonspeed * Time.deltaTime);
 
     }
 }
